b6636f7e3d98eba6725e2e75c72203c207f850d0
Anton <blabla@mail.ru>
2023-09-15 16:53:52 +0300

    otchet

--- 5.cpp
+++ 5.cpp
@@ -1,46 +1,75 @@
-#include <stdio.h>
-#include <string.h>
-#include<locale.h>
+
+#include <stdio.h> 
+#include <string.h> 
+#include<locale.h> 
+#include <stdlib.h> 
+#define h 3 
 
 int main () 
-{
-	setlocale(LC_ALL, "rus");
-	struct student 
-	{
-		char surname[15];
-		char name[15];
-		int age;
-		char falucty[15];
-	}stud[3];
+{ 
+int s = 0; 
+setlocale (LC_CTYPE,"Russian"); 
 
-	char person[15];
-	for (int i = 0; i < 3; i++)
-	{
-		printf("Введите фамилию студента: \n");
-		scanf("%s", &stud[i].surname);
-		printf("\nВведите имя студента: \n");
-		scanf("%s", &stud[i].name);
-		printf("\nВведите возраст студента: \n");
-		scanf("%d", &stud[i].age);
-		printf("\nВведите факультет студента: \n");
-		scanf("%s", &stud[i].falucty);
-	}
+struct student 
+{ 
+char surname[15]; 
+char name[15]; 
+int age; 
+char falucty[15]; 
+}stud[h]; 
 
-	printf("Введите фамилию студента, для поиска: \n");
-	scanf("%s", person);
-	
-	for (int i = 0; i < 3; i++)
-	{
-		if (strcmp(person, stud[i].surname) == 0)
-		{
-			printf("Фамилия: %s\n", stud[i].surname);
-			printf("Имя: %s\n", stud[i].name);
-			printf("Возраст: %d\n", stud[i].age);
-			printf("Факультет: %s\n", stud[i].falucty);
-			return 0;
-		}
-		else
-			printf("Нет такого пользователя!");
-	}
-	return 0;
+char person[15]; 
+for (int i = 0; i < h; i++) 
+{ 
+printf("Введите фамилию студента: \n"); 
+scanf("%s", &stud[i].surname); 
+printf("\nВведите имя студента: \n"); 
+scanf("%s", &stud[i].name); 
+printf("\nВведите возраст студента: \n"); 
+scanf("%d", &stud[i].age); 
+printf("\nВведите факультет студента: \n"); 
+scanf("%s", &stud[i].falucty); 
+} 
+
+printf("Введите фамилию ,имя, возраст, факультет студента, для поиска: \n"); 
+scanf("%s", person); 
+
+for (int i = 0; i < h; i++) 
+{ 
+if (strcmp(person, stud[i].surname) == 0) 
+{ 
+printf("Фамилия: %s\n", stud[i].surname); 
+printf("Имя: %s\n", stud[i].name); 
+printf("Возраст: %d\n", stud[i].age); 
+printf("Факультет: %s\n", stud[i].falucty); 
+s++; 
+} 
+if (strcmp(person, stud[i].name) == 0) 
+{ 
+printf("Фамилия: %s\n", stud[i].surname); 
+printf("Имя: %s\n", stud[i].name); 
+printf("Возраст: %d\n", stud[i].age); 
+printf("Факультет: %s\n", stud[i].falucty); 
+s++; 
+} 
+if (atoi(person) == stud[i].age) 
+{ 
+printf("Фамилия: %s\n", stud[i].surname); 
+printf("Имя: %s\n", stud[i].name); 
+printf("Возраст: %d\n", stud[i].age); 
+printf("Факультет: %s\n", stud[i].falucty); 
+s++; 
+} 
+if (strcmp(person, stud[i].falucty) == 0) 
+{ 
+printf("Фамилия: %s\n", stud[i].surname); 
+printf("Имя: %s\n", stud[i].name); 
+printf("Возраст: %d\n", stud[i].age); 
+printf("Факультет: %s\n", stud[i].falucty); 
+s++; 
+}  
+} 
+if (s == 0) 
+printf("Такого студента нет!\n"); 
+return 0; 
 }
